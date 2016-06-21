d5eb23c9b85ec86628d7172cff2c2dd06da824a5
Alen <alen@pwnedlist.com>
2016-06-21 15:40:03 -0500

    Updates to pl-api-client
    Signed-off-by: Alen <alen@pwnedlist.com>

--- pl-api-client.py
+++ pl-api-client.py
@@ -25,6 +25,27 @@ API_STATUS_NOT_FOUND = 404
 API_STATUS_TOO_MANY_REQUESTS = 503
 API_STATUS_QUOTA_EXCEEDED = 507
 
+# DEBUG
+import logging
+
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig() 
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+# END DEBUG
+
 
 def retry(exceptiontocheck, tries=3, delay=3, backoff=2):
     """
@@ -186,6 +207,10 @@ def run_method(args):
                 if 'password' in a:
                     a['password'] = str(api.decrypt(a['password'], args.api_secret[:16]))
         elif args.path == 'leaks.get':
+            for a in resp['accounts']:
+                if 'password' in a:
+                    a['password'] = str(api.decrypt(a['password'], args.api_secret[:16]))
+        elif args.path == 'reports.get':
             for a in resp['accounts']:
                 if 'password' in a:
                     a['password'] = str(api.decrypt(a['password'], args.api_secret[:16]))
